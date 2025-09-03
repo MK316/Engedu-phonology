449670f63cdddd3aadd0da154912175196c7ac31
MK <99416359+MK316@users.noreply.github.com>
2025-09-03 21:35:12 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,4 +1,20 @@
 import streamlit as st
 
-st.markdown("### Phonology & English Educaiton")
-st.caption("This page will get ready for Fall 2025 semester.")
+st.markdown("### Phonology & English Education")
+st.caption(
+    "Fall 2025. This space provides lecture slides, short videos, and interactive apps for "
+    "practicing English phonetics and phonology. We’ll cover articulatory and acoustic basics, "
+    "IPA skills, and classroom applications tailored for English education majors."
+)
+
+
+
+# Put this below your caption
+IMAGE_URL = "https://github.com/MK316/classmaterial/raw/main/images/bg01.png"
+st.image(IMAGE_URL, caption="Teaching is one of the best ways to learn.", width = 400, use_container_width=True)
+
+
+
+# Put this below your caption
+IMAGE_URL = "https://github.com/MK316/classmaterial/blob/main/images/engedu-qr.png"
+st.image(IMAGE_URL, caption="Course access QR (Phonology & English Education)", width = 100, use_container_width=True)
