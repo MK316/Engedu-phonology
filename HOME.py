8a3f018b9556962da7c399f717453ca5656472e7
MK <99416359+MK316@users.noreply.github.com>
2026-01-11 22:50:00 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -7,8 +7,3 @@ st.caption(
     "IPA skills, and classroom applications tailored for English education majors."
 )
 
-
-st.image("https://github.com/MK316/classmaterial/raw/main/images/bg01.png",
-             caption="Teaching is one of the best ways to learn.", width=400)
-st.image("https://github.com/MK316/classmaterial/raw/main/images/engedu-qr.png",
-             caption="Access QR", use_container_width=True)
